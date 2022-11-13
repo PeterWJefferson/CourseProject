6c784f7144128b35d951b9e1d5fbceb243971106
PeterWJefferson <peterwjefferson@gmail.com>
2022-11-13 16:50:58 -0600

    feat: Added logic to route returned tweets to the results.html page.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import json
+import TwitterAccess
 
 # template_folder is relative to where the main flask run .py file is (/api/app.py)
 app = Flask(__name__)  # , template_folder='../templates', static_url_path="/")
@@ -14,37 +15,43 @@ def index():
 def query():
     user_query = request.form['query_input']
     print(f"USER QUERY: {user_query}")
-    return render_template("results.html")
+    tweets_to_display = tweets(user_query)
+    return render_template("results.html", tweet_list=tweets_to_display)
 
-#
-# @app.route('/tweets', methods=['GET', 'POST'])
-# def tweets():
-#     api = TwitterAccess.TwitterClient()
-#     if request.method == 'POST':
-#         return login_user()
-#     elif request.method == 'GET':
-#         '''Getting the query results from Twitter and returning it to the api caller'''
-#         query = json.loads(request.data)["query"]
-#         tweets = api.get_tweets(query=query, count=100)
-#         positive_tweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
-#         return positive_tweets
-#         # return ["positive_tweets"]
-#
-#
-# @app.route('/user/<username>')
-# def show_user_profile(username=None):
-#     # username=None ensures the code run even when no name is provided
-#     return render_template('user-profile.html', username=username)
-#
-#
-# @app.route('/post/<int:post_id>')
-# def show_post(post_id):
-#     return str(post_id)
-#
-#
-# def login_user():
-#     return "You are logged in"
-#
-#
-# def serve_login_page():
-#     return "Please log in"
+
+@app.route('/tweets', methods=['GET', 'POST'])
+def tweets(user_query):
+    api = TwitterAccess.TwitterClient()
+    if request.method == 'POST':
+        '''Getting the query results from Twitter and returning it to the api caller'''
+        tweets = api.get_tweets(query=user_query, count=100)
+        positive_tweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
+        return tweets
+
+
+@app.route('/user/<username>')
+def show_user_profile(username=None):
+    # username=None ensures the code run even when no name is provided
+    return render_template('user-profile.html', username=username)
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return str(post_id)
+
+
+def clean_tweets(tweet_list):
+    """
+    Takes in a list of returned tweets from the Twitter api, cleans, adn returns them.
+    :param tweet_list: list of dictionary tweets
+    :return: list of clean tweets
+    """
+
+
+
+def login_user():
+    return "You are logged in"
+
+
+def serve_login_page():
+    return "Please log in"
